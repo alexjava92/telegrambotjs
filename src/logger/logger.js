6b468e1f514d5e9f2aec3d9996881525fdf9a388
alexjava92 <autoexpert37@yandex.ru>
2023-04-13 20:52:17 +0300

    commit final 1.5

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,13 +1,17 @@
 import winston from 'winston';
+import moment from 'moment-timezone';
+
+const moscowTimeFormat = winston.format.printf(({ timestamp, level, message }) => {
+    const moscowTime = moment(timestamp).tz('Europe/Moscow').format('YYYY-MM-DD HH:mm:ss');
+    return `[${moscowTime}] ${level}: ${message}`;
+});
 
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
         winston.format.colorize(),
         winston.format.timestamp(),
-        winston.format.printf(({ timestamp, level, message }) => {
-            return `[${timestamp}] ${level}: ${message}`;
-        })
+        moscowTimeFormat
     ),
     transports: [
         new winston.transports.Console(),
