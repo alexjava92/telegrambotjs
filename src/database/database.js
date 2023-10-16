922aee070cf3dfe544a325ac7740108a3df003a5
alexjava92 <autoexpert37@yandex.ru>
2023-10-16 17:57:03 +0300

    updated index.js

--- src/database/database.js
+++ src/database/database.js
@@ -64,7 +64,7 @@ export async function getIdUser(id) {
 
         } else {
             result = 'Текста в массиве нету';
-            // console.log('Текста в массиве нету')
+            // console.log('Текста в массиве нет')
         }
 
     } catch (error) {
